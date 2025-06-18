744b9b62e30fd60ba3400ad50b26c3aaa315853a
Hisbattleaxe <jomilojuegbayelo984@gmail.com>
2025-06-18 05:22:02 +0100

    first 2 stages

--- Js/api.js
+++ Js/api.js
@@ -3,16 +3,16 @@ const apiKey = "7b613f8b274d4f108f392431250702";
 const city = "Akure";
 const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const scrollers = document.querySelectorAll('.scroller')
 
 const date = new Date();
 let month1 = month[date.getMonth()]
-console.log(month1)
+console.log("hey i am working");
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
         const date = data.current.last_updated;
-        console.log(get.month())
         let febr = date.slice(8,11);
         document.getElementById("date").innerHTML = "Monday, " + febr + month1;
 
@@ -20,3 +20,23 @@ fetch(url)
         document.getElementById("temperature").innerHTML = Math.floor(temp) + "°C"; 
     })
     .catch(error => error)
+
+if(!window.matchMedia('(prefers-reduced-motion: reduce)').matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true);
+
+        const scrollerInner = document.querySelector('.scroller_inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach((item) => {
+            const duplicatedItems = item.cloneNode(true);
+            duplicatedItems.setAttribute("aria-hidden",true)
+            scrollerInner.appendChild(duplicatedItems);
+        });
+    });
+
+}
